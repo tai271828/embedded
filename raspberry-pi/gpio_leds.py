3287cd11192dfcbb76efb4c91d049a63e951defe
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2015-03-08 17:53:14 +0800

    typo fixing, use GPIO.output

--- raspberry-pi/gpio_leds.py
+++ raspberry-pi/gpio_leds.py
@@ -2,16 +2,22 @@
 import RPi.GPIO as GPIO
 import time
 
+print("initializing...")
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+print("initializing...done")
+time.sleep(3)
+
 for i in range(10):
     if i % 2 == 0:
-        GPIO.setup(11, GPIO.OUT)
         print("LED is on: Green")
+        GPIO.output(11, GPIO.HIGH)
         time.sleep(1)
-        GPIO.setup(11, True)
+        GPIO.output(11, GPIO.LOW)
     else:
-        GPIO.setup(12, GPIO.OUT)
         print("LED is on: RED")
+        GPIO.output(12, GPIO.HIGH)
         time.sleep(1)
-        GPIO.setup(12, True)
+        GPIO.output(12, GPIO.LOW)
 GPIO.cleanup()
