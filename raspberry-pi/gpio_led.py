9239c6ba43335e1731ea3829012bd540b416441a
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2015-03-08 17:50:33 +0800

    typo fixing, use GPIO.output to flash the led on pin 11

--- raspberry-pi/gpio_led.py
+++ raspberry-pi/gpio_led.py
@@ -3,7 +3,8 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.output(11, GPIO.HIGH)
 print("LED is on: Green")
 time.sleep(3)
 GPIO.cleanup()
